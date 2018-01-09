647a47728d67489dab4a4ef3ab2e38ed86ad034a
ASIMKHAN <ASIMKHAN@ASIMKHAN-PC>
2018-01-10 04:26:18 +0530

    1st commit
    Change-Id: I1467c6e44f7b0bb69108881820a4ed1e4f98261f
    Signed-off-by: ASIMKHAN <ASIMKHAN@ASIMKHAN-PC>

---
+++ TestGitPro/src/main/java/TestGit/TestGitPro/test1.java
@@ -0,0 +1,15 @@
+//test changes
+//test
+package TestGit.TestGitPro;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("Hello1");
+	}
+
+}
